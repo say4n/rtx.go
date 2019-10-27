f21248b01f6865a1db44c296dabbf6897ddabf69
Sayan Goswami <goswami.sayan47@gmail.com>
2019-10-27 10:11:28 +0530

    Fixes
    Correct height, width information of PPM file & for loop parameters

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 )
 
 func main() {
-	height := 400
-	width := 300
+	width := 400
+	height := 300
 
 	maxColor := 255.99
 
@@ -19,12 +19,12 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Fprintf(f, "P3\n%d %d\n%d\n", height, width, int(maxColor))
+	fmt.Fprintf(f, "P3\n%d %d\n%d\n", width, height, int(maxColor))
 
 	for ny := 0; ny < height; ny++ {
-		for nx := 0; nx < height; nx++ {
-			r := float64(nx) / float64(height)
-			g := float64(ny) / float64(width)
+		for nx := 0; nx < width; nx++ {
+			r := float64(ny) / float64(height)
+			g := float64(nx) / float64(width)
 			b := 0.5
 
 			c := gfx.NewColor(int(maxColor*r), int(maxColor*g), int(maxColor*b))
