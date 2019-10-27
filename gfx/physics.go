d7b19d2a49916a042bd386e82150f622cc01b6c4
Sayan Goswami <goswami.sayan47@gmail.com>
2019-10-27 09:49:21 +0530

    Add struct Ray
    Implements GetOrigin, GetDirection and GetPointAtParameter

---
+++ gfx/physics.go
@@ -0,0 +1,18 @@
+package gfx
+
+type Ray struct {
+	A Vector3D
+	B Vector3D
+}
+
+func (r *Ray) GetOrigin() Vector3D {
+	return r.A
+}
+
+func (r *Ray) GetDirection() Vector3D {
+	return r.B
+}
+
+func (r *Ray) GetPointAtParameter(t float64) Vector3D {
+	return r.A.Add(r.B.Scale(t))
+}
